513ef9b2bac4757f92ee2999805924de9ed1b2ae
abhishen113 <abhishen99@gmail.com>
2024-01-20 14:29:41 +0530

    update

--- firstnextjs/src/app/components/postCard/postCard.jsx
+++ firstnextjs/src/app/components/postCard/postCard.jsx
@@ -9,23 +9,26 @@ import Link from "next/link"
 
 
 
-const PostCard = () => {
+const PostCard = ({post}) => {
 
   return (
 
     <div className={style.container}>
 
-      <div className={style.top}>
+      
+     <div className={style.top}>
         <div className={style.imgContainer}>
-          <Image className={style.img} src="https://images.pexels.com/photos/327393/pexels-photo-327393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill />
+          <Image src="https://images.pexels.com/photos/327393/pexels-photo-327393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  className={style.img} alt="" fill  />
         </div>
         <span className={style.date}> 01.01.2024 </span>
 
-      </div>
+
+      </div> 
+ 
       <div className={style.buttom} >
-        <h1 className={style.title}>Title</h1>
-        <p className={style.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit facilis, nihil totam natus itaque ratione incidunt cumque eaque voluptatem eos, molestiae illo dolorum corporis voluptates blanditiis veniam illum optio nisi.</p>
-        <Link className={style.link} href="/blog/post">READ MORE</Link>
+        <h1 className={style.title}>{post.title}</h1>
+        <p className={style.desc}> {post.body}</p>
+        <Link className={style.link} href={ `/blog/${post.id}` }>READ MORE</Link>
 
       </div>
 
@@ -33,7 +36,7 @@ const PostCard = () => {
     </div>
 
 
-  )
-}
+  );
+};
 
 export default PostCard
